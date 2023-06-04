71748b0c13af68cfdd084d035d742c99970f0866
Jan Pokorny <pokorny@protonmail.com>
2023-06-04 17:36:03 +0200

    Rotate screen fix

--- RotateScreen.sh
+++ RotateScreen.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+
+# Rotates screen left if it is normal or vice versa.
+
+randr=gnome-randr # alternative to randr on wayland using GNOME Manjaro
 file=~/.screen_rotation
-if [ -f "$file" ]
+screen_name=eDP-1
+
+if [ $# -eq 0 ]
 then
-    echo 0 > file 
+    prev_rotation=$("$randr" | grep rotation)
+    echo $prev_rotation
+    echo ==============
+    case "$prev_rotation" in
+
+        *normal*)
+            rotation=left
+            ;;
+
+        *)
+            rotation=normal
+            ;;
+    esac
+elif [ $# -eq 1 ]
+then
+    rotation="$1"
+else
+    echo Wrong number of arguments.
+    exit 1
 fi
 
-rotation=$(cat $file)
-
-xrandr -o $rotation
-
-echo $((rotation^1)) > $file
+"$randr" --output "$screen_name" --rotate $rotation
