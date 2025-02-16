36a5a6f65fcf4510e34254d156fa8ffae9bfd4f9
Jan Pokorný <pokorny@protonmail.com>
2025-02-16 11:34:22 +0100

    gitsync

---
+++ gitsync.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+DIR=~/.gitsync/
+
+if [ ! -d "$DIR" ]
+ then
+  echo "$DIR does not exist, creating."
+  mkdir $DIR
+  mkdir $DIR/r/
+  mkdir $DIR/rw/
+fi
+
+if [ $# -ge "2" ] && [ "$1" = "register" ]
+then 
+    ln -s "$(pwd)" "$DIR/$2"
+    exit 0
+fi
+
+shopt -s dotglob 
+
+for dir in "$DIR/r/"*
+do
+    echo $dir
+    cd $dir
+    git pull --rebase
+done 
+
+for dir in "$DIR/rw/"*
+do
+    echo $dir
+    cd $dir
+    git pull --rebase
+    git add .
+    git commit -m "Auto-sync: $(date)" || echo "Nothing to commit".
+done 
+
